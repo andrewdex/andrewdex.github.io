50312417335356ef16ec9c7653d90eb3bc26d036
Andrew dEX <andrewdexis@gmail.com>
2014-11-08 21:00:15 +0530

    updated

--- js/andrewdex.js
+++ js/andrewdex.js
@@ -2,7 +2,7 @@ var phrase;
 phrase = "<div class='legend'>{ Dilusha Gonagala }</div>";
 phrase += "<div class='line marginTop10'></div>";
 phrase += "<div class='legend marginTop10'>AKA { Andrew dEX @andrewdex}</div>";
-phrase += "<div class='mottohead marginTop10'>The Fullstack Software Engineer...</div>";
+phrase += "<div class='mottohead marginTop10'>I am a Fullstack FrontEnd Software Engineer...</div>";
 // phrase += "<div class='await marginTop10'>Coming Soon !...</div>";
 phrase += "<div class='legend marginTop10'><a href='http://twiter.com/andrewdex'>@andrewdex</a></div>";
 phrase += "<div align='center'><img src='images/me.jpg' width='150' height='150'/></div>";
